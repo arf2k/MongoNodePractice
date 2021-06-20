c32e807868116d524a1b16ec688f6c58eaee10ca
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-06-20 19:35:03 -0400

    Use a second method to make multiple entries

--- Mongodb.js
+++ Mongodb.js
@@ -6,8 +6,7 @@ const MongoClient = mongodb.MongoClient;
 const connectionUrl = "mongodb://127.0.0.1:27017";
 const dbFile = "MongoPractice";
 
-
-//Create DB entry 
+//Create DB entry
 MongoClient.connect(
   connectionUrl,
   { useNewUrlParser: true },
@@ -15,18 +14,37 @@ MongoClient.connect(
     if (error) {
       return console.log("Can't connect to db");
     }
+
     const db = client.db(dbFile);
-    db.collection("pets").insertOne({
-      name: "Miss Maeby",
-      animal: "cat",
-    }, (error, result) => {
-         if(error){
-              return console.log("unable to add entry")
-         }
-         console.log(result.ops)
-    });
+
+    //     db.collection("pets").insertOne({
+    //       name: "Miss Maeby",
+    //       animal: "cat",
+    //     }, (error, result) => {
+    //          if(error){
+    //               return console.log("unable to add entry")
+    //          }
+    //          console.log(result.ops)
+    //     });
+    //   }
+    // )
+
+    //insertOne is asynchronous so add callback for insertions- this is an example of the READ in CRUD
+
+    db.collection("pets").insertMany(
+      [
+        { name: "Toast", animal: "dog" },
+        {
+          name: "Kayla",
+          animal: "cat",
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("unable to add entries to db");
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );
-
-//insertOne is asynchronous so add callback for insertions- this is an example of the READ in CRUD 
-

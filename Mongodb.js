4cbd50f10109f1ab62632f913e3ada9b8d5a3ff0
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-06-21 16:35:27 -0400

    Add new cat to database

--- Mongodb.js
+++ Mongodb.js
@@ -6,7 +6,6 @@ const MongoClient = mongodb.MongoClient;
 const connectionUrl = "mongodb://127.0.0.1:27017";
 const dbFile = "MongoPractice";
 
-//Create DB entry
 MongoClient.connect(
   connectionUrl,
   { useNewUrlParser: true },
@@ -17,34 +16,45 @@ MongoClient.connect(
 
     const db = client.db(dbFile);
 
-    //     db.collection("pets").insertOne({
-    //       name: "Miss Maeby",
-    //       animal: "cat",
-    //     }, (error, result) => {
-    //          if(error){
-    //               return console.log("unable to add entry")
-    //          }
-    //          console.log(result.ops)
-    //     });
-    //   }
-    // )
+//         db.collection("pets").insertOne({
+//           name: "Miss Maeby",
+//           animal: "cat",
+//         }, (error, result) => {
+//              if(error){
+//                   return console.log("unable to add entry")
+//              }
+//              console.log(result.ops)
+//         });
+//       }
+//     )
 
     //insertOne is asynchronous so add callback for insertions- this is an example of the READ in CRUD
 
-    db.collection("pets").insertMany(
-      [
-        { name: "Toast", animal: "dog" },
-        {
-          name: "Kayla",
-          animal: "cat",
-        },
-      ],
-      (error, result) => {
-        if (error) {
-          return console.log("unable to add entries to db");
-        }
-        console.log(result.ops);
-      }
-    );
+//     db.collection("pets").insertMany(
+//       [
+//         { name: "Toast", animal: "dog" },
+//         {
+//           name: "Kayla",
+//           animal: "cat",
+//         },
+//       ],
+//       (error, result) => {
+//         if (error) {
+//           return console.log("unable to add entries to db");
+//         }
+//         console.log(result.ops);
+//       }
+//     );
+  
+
+    db.collection("pets").insertOne({
+         name: "Muffin",
+         animal: "cat"
+    }, (error, result) => {
+         if(error) {
+              return console.log("can't add")
+         }
+         console.log(result.ops)
+    })
   }
-);
+)

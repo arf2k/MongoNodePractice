c10a2399a170fbf93c8dab48237326a4449bf201
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-06-22 14:01:24 -0400

    Remove individual insertion operations

--- Mongodb.js
+++ Mongodb.js
@@ -5,80 +5,36 @@ const MongoClient = mongodb.MongoClient;
 
 const connectionUrl = "mongodb://127.0.0.1:27017";
 const dbFile = "MongoPractice";
-const ObjId = mongodb.ObjectID
 
-// const { MongoClient, ObjectID } = require("mongodb")
-// const id = new ObjectID()
-// console.log(id)
 
-// MongoClient.connect(
-//   connectionUrl,
-//   { useNewUrlParser: true },
-//   (error, client) => {
-//     if (error) {
-//       return console.log("Can't connect to db");
-//     }
 
-//     const db = client.db(dbFile);
+MongoClient.connect(
+  connectionUrl,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Can't connect to db");
+    }
 
-//         db.collection("pets").insertOne({
-//           name: "Miss Maeby",
-//           animal: "cat",
-//         }, (error, result) => {
-//              if(error){
-//                   return console.log("unable to add entry")
-//              }
-//              console.log(result.ops)
-//         });
-//       }
-//     )
+    const db = client.db(dbFile);
+
+        db.collection("pets").insertOne({
+          _id: id,
+          //dont need this bc mongo does it on its own
+          name: "Biscuit",
+          animal: "hamster",
+        }, (error, result) => {
+             if(error){
+                  return console.log("unable to add entry")
+             }
+             console.log(result.ops)
+        });
+      }
+    )
 
     //insertOne is asynchronous so add callback for insertions- this is an example of the READ in CRUD
 
-//     db.collection("pets").insertMany(
-//       [
-//         { name: "Toast", animal: "dog" },
-//         {
-//           name: "Kayla",
-//           animal: "cat",
-//         },
-//       ],
-//       (error, result) => {
-//         if (error) {
-//           return console.log("unable to add entries to db");
-//         }
-//         console.log(result.ops);
-//       }
-//     );
-  
 
-//     db.collection("pets").insertOne({
-//          name: "Muffin",
-//          animal: "cat"
-//     }, (error, result) => {
-//          if(error) {
-//               return console.log("can't add")
-//          }
-//          console.log(result.ops)
-//     })
-//   }
-// )
 
-// db.collection("pets").insertMany([
-//   { name: "Mouse",
-//   animal: "mouse"
-    
-//   }, {
-//     name: "Duncan",
-//     animal: "kangaroo"
-//   },
+///CRUD read operation 
 
-// ], (error, result) => {
-//   if(error){
-//     return console.log("Unable to add animals")
-//   }
-//   console.log(result.ops)
-// })
-
-//   }
-// )

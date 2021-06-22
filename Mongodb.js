758e73b16fc0c7ef8b1c02539716dbe8235dc8d2
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-06-22 14:06:11 -0400

    Successful Read crud action

--- Mongodb.js
+++ Mongodb.js
@@ -8,29 +8,29 @@ const dbFile = "MongoPractice";
 
 
 
-MongoClient.connect(
-  connectionUrl,
-  { useNewUrlParser: true },
-  (error, client) => {
-    if (error) {
-      return console.log("Can't connect to db");
-    }
+// MongoClient.connect(
+//   connectionUrl,
+//   { useNewUrlParser: true },
+//   (error, client) => {
+//     if (error) {
+//       return console.log("Can't connect to db");
+//     }
 
-    const db = client.db(dbFile);
+//     const db = client.db(dbFile);
 
-        db.collection("pets").insertOne({
-          _id: id,
-          //dont need this bc mongo does it on its own
-          name: "Biscuit",
-          animal: "hamster",
-        }, (error, result) => {
-             if(error){
-                  return console.log("unable to add entry")
-             }
-             console.log(result.ops)
-        });
-      }
-    )
+//         db.collection("pets").insertOne({
+//           _id: id,
+//           //dont need this bc mongo does it on its own
+//           name: "Biscuit",
+//           animal: "hamster",
+//         }, (error, result) => {
+//              if(error){
+//                   return console.log("unable to add entry")
+//              }
+//              console.log(result.ops)
+//         });
+//       }
+//     )
 
     //insertOne is asynchronous so add callback for insertions- this is an example of the READ in CRUD
 
@@ -38,3 +38,20 @@ MongoClient.connect(
 
 ///CRUD read operation 
 
+MongoClient.connect(
+  connectionUrl,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Can't connect to db");
+    }
+    const db = client.db(dbFile);
+
+    db.collection("pets").findOne({name: "Miss Maeby"}, (error, pet) => {
+      if(error) {
+        return console.log("unable to retrieve")
+      }
+      console.log(pet)
+    })
+  }
+)

c5528b161994e67073859f13e2072197d651df29
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-06-24 17:23:56 -0400

    Uncommenting to retest read function

--- Mongodb.js
+++ Mongodb.js
@@ -6,37 +6,33 @@ const MongoClient = mongodb.MongoClient;
 const connectionUrl = "mongodb://127.0.0.1:27017";
 const dbFile = "MongoPractice";
 
+MongoClient.connect(
+  connectionUrl,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Can't connect to db");
+    }
 
+    const db = client.db(dbFile);
 
-// MongoClient.connect(
-//   connectionUrl,
-//   { useNewUrlParser: true },
-//   (error, client) => {
-//     if (error) {
-//       return console.log("Can't connect to db");
-//     }
+        db.collection("pets").insertOne({
+          _id: id,
+          //dont need this bc mongo does it on its own
+          name: "Biscuit",
+          animal: "hamster",
+        }, (error, result) => {
+             if(error){
+                  return console.log("unable to add entry")
+             }
+             console.log(result.ops)
+        });
+      }
+    )
 
-//     const db = client.db(dbFile);
+insertOne is asynchronous so add callback for insertions- this is an example of the READ in CRUD
 
-//         db.collection("pets").insertOne({
-//           _id: id,
-//           //dont need this bc mongo does it on its own
-//           name: "Biscuit",
-//           animal: "hamster",
-//         }, (error, result) => {
-//              if(error){
-//                   return console.log("unable to add entry")
-//              }
-//              console.log(result.ops)
-//         });
-//       }
-//     )
-
-    //insertOne is asynchronous so add callback for insertions- this is an example of the READ in CRUD
-
-
-
-///CRUD READ operation 
+/CRUD READ operation
 
 MongoClient.connect(
   connectionUrl,
@@ -54,10 +50,10 @@ MongoClient.connect(
     //   console.log(pet)
     // })
 
-    db.collection("pets").find({animal: "cat"}).toArray((error, pets) => {
-      console.log(pets)
-    })
+    db.collection("pets")
+      .find({ animal: "cat" })
+      .toArray((error, pets) => {
+        console.log(pets);
+      });
   }
-
-
-)
+);

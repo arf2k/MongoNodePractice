16d0cdca155cdeddd816e18b1e963bc893d62983
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-06-27 17:08:20 -0400

    Update functionality using promises and object id

--- Mongodb.js
+++ Mongodb.js
@@ -6,33 +6,33 @@ const MongoClient = mongodb.MongoClient;
 const connectionUrl = "mongodb://127.0.0.1:27017";
 const dbFile = "MongoPractice";
 
-MongoClient.connect(
-  connectionUrl,
-  { useNewUrlParser: true },
-  (error, client) => {
-    if (error) {
-      return console.log("Can't connect to db");
-    }
+// MongoClient.connect(
+//   connectionUrl,
+//   { useNewUrlParser: true },
+//   (error, client) => {
+//     if (error) {
+//       return console.log("Can't connect to db");
+//     }
 
-    const db = client.db(dbFile);
+//     const db = client.db(dbFile);
 
-        db.collection("pets").insertOne({
-          _id: id,
-          //dont need this bc mongo does it on its own
-          name: "Biscuit",
-          animal: "hamster",
-        }, (error, result) => {
-             if(error){
-                  return console.log("unable to add entry")
-             }
-             console.log(result.ops)
-        });
-      }
-    )
+//     db.collection("pets").insertOne({
+//       // _id: id,
+//       //dont need this bc mongo does it on its own
+//       name: "Biscuit",
+//       animal: "hamster",
+//     }, (error, result) => {
+//          if(error){
+//               return console.log("unable to add entry")
+//          }
+//          console.log(result.ops)
+//     });
+//   }
+// )
 
-insertOne is asynchronous so add callback for insertions- this is an example of the READ in CRUD
+//insertOne is asynchronous so add callback for insertions- this is an example of the READ in CRUD
 
-/CRUD READ operation
+//CRUD READ operation
 
 MongoClient.connect(
   connectionUrl,
@@ -50,10 +50,33 @@ MongoClient.connect(
     //   console.log(pet)
     // })
 
-    db.collection("pets")
-      .find({ animal: "cat" })
-      .toArray((error, pets) => {
-        console.log(pets);
+    //     db.collection("pets")
+    //       .find({ animal: "cat" })
+    //       .toArray((error, pets) => {
+    //         console.log(pets);
+    //       });
+    //   }
+    // );
+
+    //UPDATING using promises (instead of callbacks)
+
+    const updatePromise = 
+    db.collection("pets").updateOne(
+      {
+        _id: new mongodb.ObjectID("60d223cb154e36c5e7aa0429"),
+      },
+      {
+        $set: {
+          name: "Wilson",
+        },
+      }
+    );
+    updatePromise
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
       });
   }
 );

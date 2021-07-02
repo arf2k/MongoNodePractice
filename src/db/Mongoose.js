7027e0e367137024b56568e1edb0ebe568be3004
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-07-02 16:47:45 -0400

    Create Task model and sample entry

--- src/db/Mongoose.js
+++ src/db/Mongoose.js
@@ -1,25 +1,50 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/sample-api", {
-     useNewUrlParser: true,
-     useCreateIndex: true
-})
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
 const User = mongoose.model("User", {
-     name: {
-          type: String
-     },
-     age: {
-          type: Number
-     }
-})
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
 
 const me = new User({
-     name: "Xander",
-     age: "turtle"
-})
+  name: "Xander",
+  age: "turtle",
+});
 
-me.save().then(() => {
-console.log(me)
-}).catch((error) => console.log("Error!", error))
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => console.log("Error!", error));
 //returns promise
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+  },
+});
+
+const task = new Task({
+  description: "Buy gooseberries",
+  completed: false,
+});
+
+task
+  .save()
+  .then(() => {
+    console.log(task);
+  })
+  .catch((error) => {
+    console.log(error);
+  });

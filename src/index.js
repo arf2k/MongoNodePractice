1d6e6fa6cea5efa3fe7c04f22a59e259626fff65
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-07-04 17:39:17 -0400

    Create models folder and user file and move info for user model from mongoose file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(express.json())
 app.listen(port, () => {
-     console.log(`you're connected to port ${port}`)})
+  console.log(`you're connected to port ${port}`);
+});
+
+app.post("/users", (req, res) => {
+     console.log(req.body)
+     res.send("testing")
+})
+
